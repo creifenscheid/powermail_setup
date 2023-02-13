8aaf35be7d9e4449e719e168a1735f902aafb22d
creifenscheid <58692838+creifenscheid@users.noreply.github.com>
2023-02-13 10:32:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## TYPO3 Extension "xxx"
+
+## Features
+- ...
+
+## Installation
+
+Install this extension via `composer req creifenscheid/webaim-wave` or download it from the [TYPO3 Extension Repository](https://extensions.typo3.org/extension/webaim_wave/) and activate
+the extension in the Extension Manager of your TYPO3 installation.
+
+### Support
+I don't want your money or anything else.
+I am doing this for fun, with heart and to improve my coding skills.
+Constructive critisism is very welcome.
+If you want to contribute, feel free to do so.
+Thank you!
---
+++ ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2023 C. Reifenscheid
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'xxx',
+    'description' => 'xxx',
+    'category' => 'be',
+    'author' => 'C. Reifenscheid',
+    'version' => '11.0.0',
+    'state' => 'alpha',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-11.5.99'
+        ] 
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'CReifenscheid\\xxx\\' => 'Classes'
+        ]
+    ]
+];
